a8f63b1db243e2ce83c37d241ca3bcf5e3ade7d6
Rahul-codehub <rahulyadavrasalpur@zohomail.in>
2026-08-07 14:57:38 +0530

    npm build completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Chatbot from "@/components/Chatbot";
-// import CursorGlow from "@/components/CursorGlow";
 import LoadingScreen from "@/components/LoadingScreen";
 
 import Hero from "@/sections/Hero";
@@ -81,7 +80,7 @@ export default function Home() {
 
       </div>
 
-      {/* <CursorGlow /> */}
+      
 
       <Navbar />
 
